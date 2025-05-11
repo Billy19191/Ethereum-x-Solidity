c27c8b2cd243c01468eaec9ea41b6883937f6301
Billy19191 <bill.ratchanon@gmail.com>
2025-05-12 02:12:56 +0700

    ✨  feat: implement enter and random functions in Lottery contract

--- projects/lottery/contracts/Lottery.sol
+++ projects/lottery/contracts/Lottery.sol
@@ -4,8 +4,23 @@ pragma solidity 0.8.30;
 
 contract Lottery {
     address public manager;
+    address[] public players;
 
-    constructor(){
+    constructor() {
         manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > 0.001 ether);
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint) {
+        return
+            uint(
+                keccak256(
+                    abi.encodePacked(block.prevrandao, block.timestamp, players)
+                )
+            );
     }
 }
